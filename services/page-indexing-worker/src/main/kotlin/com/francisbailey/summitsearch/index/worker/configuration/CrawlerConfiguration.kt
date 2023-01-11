e6cdd498356fb3c850232e74fe870f941b36e4bd
Francis Bailey <francisbailey@protonmail.com>
2023-01-10 21:19:28 -0800

    Add more charset overrides

--- services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/configuration/CrawlerConfiguration.kt
+++ services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/configuration/CrawlerConfiguration.kt
@@ -8,7 +8,9 @@ open class CrawlerConfiguration {
 
     val charsetOverride = hashMapOf(
         "goldenscrambles.ca" to Charsets.ISO_8859_1,
-        "sonnybou.ca" to Charsets.ISO_8859_1
+        "sonnybou.ca" to Charsets.ISO_8859_1,
+        "sverdina.com" to Charsets.ISO_8859_1,
+        "ericsbasecamp.net" to Charsets.ISO_8859_1
     )
 
 }
