8d17b009b9919bf43df24c007f29f29ee9304d3a
Francis Bailey <francisbailey@protonmail.com>
2023-01-16 17:59:27 -0800

    Add image caption extension on element selection

--- services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/extension/ElementExtension.kt
+++ services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/extension/ElementExtension.kt
@@ -5,3 +5,31 @@ import org.jsoup.nodes.Element
 fun Element.getLinks(): List<String> {
     return this.select("a[href]").map { it.attr("abs:href") }
 }
+
+fun Element.src(): String? {
+    return this.attr("src")
+}
+
+fun Element.getCaptionedImages(): List<CaptionedImage> {
+    val figures = this.select("figure") as List<Element>
+
+    return figures.map {
+        val image = it.select("img[src~=(?i)\\.(png|jpe?g)]").first()
+        val caption = it.select("figcaption").text()
+        val src = image?.src()
+
+        src to caption
+    }.filterNot {
+        it.first.isNullOrBlank() || it.second.isNullOrBlank()
+    }.map {
+        CaptionedImage(
+            caption = it.second,
+            imageSrc = it.first!!
+        )
+    }
+}
+
+data class CaptionedImage(
+    val caption: String,
+    val imageSrc: String
+)
---
+++ services/page-indexing-worker/src/test/kotlin/com/francisbailey/summitsearch/index/worker/extension/ElementExtensionTest.kt
@@ -0,0 +1,54 @@
+package com.francisbailey.summitsearch.index.worker.extension
+
+import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ElementExtensionTest {
+
+
+    @Test
+    fun `only fetches images wrapped by a figure and with a figcaption`() {
+        val html = """
+           <html>
+                <body>
+                    <p>Test <img id="bad-image" src="some-source" /></p>
+                    <figure>
+                        <img id="good-image" src="a-good-source.png" />
+                        <figcaption>Hello world</figcaption>
+                    </figure>
+                    <figure>
+                        <img id="bad-image-without-caption" src="some-other-source" />
+                    </figure>
+                </body>
+           </html>
+        """
+
+        val document = Jsoup.parse(html)
+        val images = document.getCaptionedImages()
+
+        assertEquals(1, images.size)
+        assertEquals("a-good-source.png", images.first().imageSrc)
+    }
+
+    @Test
+    fun `skips images that do not have a png or jpeg extension`() {
+        val html = """
+           <html>
+                <body>
+                    <figure>
+                        <img id="good-image" src="a-good-source.gif" />
+                        <figcaption>Hello world</figcaption>
+                    </figure>
+                </body>
+           </html>
+        """
+
+        val document = Jsoup.parse(html)
+        val images = document.getCaptionedImages()
+
+        assertTrue(images.isEmpty())
+    }
+
+}
