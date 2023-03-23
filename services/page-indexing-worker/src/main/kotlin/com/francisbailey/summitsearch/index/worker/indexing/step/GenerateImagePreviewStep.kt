042c22ddbf16f5068f4503cafcdc1b4b3874bd6e
francisbaileyh <francisbailey@protonmail.com>
2023-03-23 12:20:58 -0700

    Add minimum image dimension enforcement on image indexing

--- services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/indexing/step/GenerateImagePreviewStep.kt
+++ services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/indexing/step/GenerateImagePreviewStep.kt
@@ -11,13 +11,22 @@ class GenerateImagePreviewStep: Step<ImmutableImage> {
 
     override fun process(entity: PipelineItem<ImmutableImage>, monitor: PipelineMonitor): PipelineItem<ImmutableImage> {
         log.info { "Attempting to scale: ${entity.task.details.entityUrl}" }
+
+        if (entity.payload!!.height < MIN_HEIGHT || entity.payload!!.width < MIN_WIDTH) {
+            log.warn { "Image: ${entity.task.details.entityUrl} size is too small. Skipping" }
+            return entity.apply { this.continueProcessing = false }
+        }
+
         return entity.apply {
             payload = payload?.scaleToHeight(DEFAULT_HEIGHT)
+
             continueProcessing = true
         }
     }
 
     companion object {
         const val DEFAULT_HEIGHT = 200
+        const val MIN_WIDTH = 150
+        const val MIN_HEIGHT = DEFAULT_HEIGHT
     }
 }
--- services/page-indexing-worker/src/test/kotlin/com/francisbailey/summitsearch/index/worker/indexing/GenerateImagePreviewStepTest.kt
+++ services/page-indexing-worker/src/test/kotlin/com/francisbailey/summitsearch/index/worker/indexing/GenerateImagePreviewStepTest.kt
@@ -2,37 +2,50 @@ package com.francisbailey.summitsearch.index.worker.indexing
 
 import com.francisbailey.summitsearch.index.worker.indexing.step.GenerateImagePreviewStep
 import com.sksamuel.scrimage.ImmutableImage
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
 
 class GenerateImagePreviewStepTest: StepTest() {
 
-    private val image = mock<ImmutableImage>()
+    private val image = ImmutableImage.create(
+        GenerateImagePreviewStep.MIN_WIDTH + 5,
+        GenerateImagePreviewStep.MIN_HEIGHT + 5,
+    )
 
     private val step = GenerateImagePreviewStep()
 
-    private val item = PipelineItem<ImmutableImage>(
+    private val item = PipelineItem(
         payload = image,
         task = defaultIndexTask
     )
 
     @Test
-    fun `should scale image to 92 pixels in height`() {
+    fun `should scale image to 200 pixels in height`() {
         val modifiedItem = step.process(item, monitor)
 
-        verify(image).scaleToHeight(200)
+        assertEquals(GenerateImagePreviewStep.DEFAULT_HEIGHT, modifiedItem.payload?.height)
         assertTrue(modifiedItem.continueProcessing)
     }
 
     @Test
-    fun `should stop processing if scaling fails`() {
-        whenever(image.scaleToHeight(any())).thenThrow(RuntimeException("Test"))
-        assertThrows<RuntimeException> { step.process(item, monitor) }
+    fun `aborts processing if image is less than min height`() {
+        item.payload = ImmutableImage.create(
+            GenerateImagePreviewStep.MIN_WIDTH,
+            GenerateImagePreviewStep.MIN_HEIGHT - 1
+        )
+        val modifiedItem = step.process(item, monitor)
+
+        assertFalse(modifiedItem.continueProcessing)
+    }
+
+    @Test
+    fun `aborts processing if image is less than min width`() {
+        item.payload = ImmutableImage.create(
+            GenerateImagePreviewStep.MIN_WIDTH - 1,
+            GenerateImagePreviewStep.MIN_HEIGHT
+        )
+        val modifiedItem = step.process(item, monitor)
+
+        assertFalse(modifiedItem.continueProcessing)
     }
 }
