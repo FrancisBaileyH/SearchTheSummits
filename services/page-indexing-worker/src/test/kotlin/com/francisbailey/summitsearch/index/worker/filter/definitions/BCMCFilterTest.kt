0292c266a7cc5be9d7b8245078fd138911fbe9d3
francisbaileyh <francisbailey@protonmail.com>
2023-04-09 12:19:36 -0700

    Add more tests to BCMC filter

--- services/page-indexing-worker/src/test/kotlin/com/francisbailey/summitsearch/index/worker/filter/definitions/BCMCFilterTest.kt
+++ services/page-indexing-worker/src/test/kotlin/com/francisbailey/summitsearch/index/worker/filter/definitions/BCMCFilterTest.kt
@@ -16,7 +16,8 @@ class BCMCFilterTest: FilterTest() {
             "https://bcmc.ca/m/docs/view/BCMC-Newsletter-November-2001-2012-03-17",
             "https://bcmc.ca/media/newsletters/BCMC Newsletter 2001-11.pdf",
             "https://bcmc.ca/m/docs/view/The-B-C-Mountaineer-2016",
-            "https://bcmc.ca/media/newsletters/BC Mountaineer 2004.pdf"
+            "https://bcmc.ca/media/newsletters/BC Mountaineer 2004.pdf",
+            "https://bcmc.ca/m/docs/?t=1&search_mode=latest&sort=latest&page=1&per_page=100"
         )
 
         verifyFilter(linkDiscoveryFilterService, expectedToSkip, expectedNotToSkip)
