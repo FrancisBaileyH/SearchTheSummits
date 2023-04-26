ac46db63a79d8043ce2a6d7f885045c0c071b2de
francisbaileyh <francisbailey@protonmail.com>
2023-04-26 15:33:38 -0700

    Add blog spot images to submission step

--- services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/extractor/strategy/DefaultImageExtractorStrategy.kt
+++ services/page-indexing-worker/src/main/kotlin/com/francisbailey/summitsearch/index/worker/extractor/strategy/DefaultImageExtractorStrategy.kt
@@ -1,14 +1,14 @@
 package com.francisbailey.summitsearch.index.worker.extractor.strategy
 
-import com.francisbailey.summitsearch.index.worker.extension.CaptionedImage
-import com.francisbailey.summitsearch.index.worker.extension.getDlCaptionedImages
-import com.francisbailey.summitsearch.index.worker.extension.getFigCaptionedImages
-import com.francisbailey.summitsearch.index.worker.extension.getWPCaptionedImages
+import com.francisbailey.summitsearch.index.worker.extension.*
 import com.francisbailey.summitsearch.index.worker.extractor.ContentExtractorStrategy
 import org.jsoup.nodes.Document
 
 class DefaultImageExtractorStrategy: ContentExtractorStrategy<List<CaptionedImage>> {
     override fun extract(document: Document): List<CaptionedImage> {
-        return document.getWPCaptionedImages() + document.getFigCaptionedImages() + document.getDlCaptionedImages()
+        return document.getWPCaptionedImages() +
+            document.getFigCaptionedImages() +
+            document.getDlCaptionedImages() +
+            document.getBlogSpotCaptionedImages()
     }
 }
